d47a70e3f0b16ca16e8ad844cd4d2c9962db87e1
aaalb <albert1alibe@gmail.com>
2025-01-02 14:56:12 +0100

    merge

--- backend/src/controller/auction.controller.ts
+++ backend/src/controller/auction.controller.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from "express";
 import {createAuction, deleteAuction, searchAuctionById, searchAuctions, getUserAuctions} from "../service/auction.service";
 import logger from "../utils/logger";
-import {CreateAuctionInput, DeleteAuctionInput, GetAuctionInput, SearchAuctionInput} from "../schema/auction.schema";
+import {CreateAuctionInput, SearchAuctionInput} from "../schema/auction.schema";
 import {createBook, searchBookByISBN} from "../service/book.service";
 import {omit} from "lodash";
 import sanitize from "mongo-sanitize";
@@ -52,11 +52,11 @@ export async function getAuctionHandler(req: Request, res: Response) {
     const auction = await searchAuctionById({auctionId: auctionId});
 
     if (!auction) {
-        res.sendStatus(404);
+        res.status(404).send({message: 'Auction not found'});
         return;
     }
 
-    res.send(auction);
+    res.status(200).send(auction);
 }
 
 export async function getAllAuctionHandler(req: Request, res: Response) {
@@ -81,25 +81,21 @@ export async function getAllAuctionHandler(req: Request, res: Response) {
     res.send(filteredAuctions);
 }
 
-export async function deleteAuctionHandler(req: Request<DeleteAuctionInput['body']>, res: Response) {
-    const auctionId = req.body.auctionId;
-    const userId = res.locals.user._id;
+export async function deleteAuctionHandler(req: Request<{auctionId: string}>, res: Response) {
+const { auctionId } = req.params;
 
-    const auction = await searchAuctionById({auctionId: auctionId});
+
+    const auction = await searchAuctionById(auctionId);
 
     if (!auction) {
         res.sendStatus(404);
         return;
     }
 
-    if (auction.seller != userId) {
-        res.sendStatus(403);
-        return;
-    }
-
     const deletedAuction = await deleteAuction({auctionId});
 
-    res.send(deletedAuction);
+    res.status(200).send(deletedAuction);
+    return;
 }
 
 export async function searchAuctionHandler(req: Request<SearchAuctionInput['body']>, res: Response) {
--- backend/src/routes/auction/routes/info.routes.ts
+++ backend/src/routes/auction/routes/info.routes.ts
@@ -5,8 +5,16 @@ import { searchAuctionSchema } from '../../../schema/auction.schema';
 
 const router = express.Router();
 
-router.get('/all', AuctionController.getAllAuctionHandler);
 router.post('/search', [validateResource(searchAuctionSchema)], AuctionController.searchAuctionHandler);
 router.get('/:auctionId', AuctionController.getAuctionHandler);
+/*
+router.get('/auctions', requireUser, AuctionController.getUserAuctionsHandler);
+router.get('/all', AuctionController.getAllAuctionHandler);
+router.post('/search', [validateResource(searchAuctionSchema)], AuctionController.searchAuctionHandler);
+*/
+
+
+//tutte le aste, con filtri query params
+router.get('/', AuctionController.getAllAuctionHandler);
 
 export default router;
